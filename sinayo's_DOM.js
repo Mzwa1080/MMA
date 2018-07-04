8cb468db35e4e7b06353561f8794cd0b7c6192b0
Mzwa <mshawnlouw@gmail.com>
2018-07-04 12:34:57 +0200

    work in  progress 2

--- sinayo's_DOM.js
+++ sinayo's_DOM.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-  var elems = document.querySelectorAll('.sidenav');
-  var instances = M.Sidenav.init(elems);
-});    
+  var elems = document.querySelectorAll('.slider');
+  var instances = M.Slider.init(elems);
+
+});
